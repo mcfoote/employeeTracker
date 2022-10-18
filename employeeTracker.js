5f2788c78d743aeea0709c2bf3ab3ffa88cbc188
Mitchell Foote <mcfoote321@gmail.com>
2022-10-18 00:12:12 -0400

    outline js

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,3 +1,35 @@
 const consoleTable = require('console.table');
 const sql = require('mysql2');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+
+});
+
+function employeeTracker() {
+
+};
+
+function addDepartment() {
+
+};
+
+function addRole() {
+
+};
+
+function addEmployee() {
+
+};
+
+function viewDeparmtents() {
+
+};
+
+function viewRoles() {
+
+};
+
+function viewEmployees() {
+
+};
