3a85b617df92a92797a589b674ad3ebed1ae4884
Mitchell Foote <mcfoote321@gmail.com>
2022-10-18 23:29:10 -0400

    console ascii art title

--- employeeTracker.js
+++ employeeTracker.js
@@ -3,12 +3,38 @@ const sql = require('mysql2');
 const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+
+    host: 'localhost',
     port: 3306,
-    user: "root",
-    password: "",
-    database: "employee_db"
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+
 });
+
+connection.connect(function(err) {
+
+    if(err) throw err;
+
+    console.log('Successfully Connected as ' + connection.threadId);
+
+    console.log('  ______                 _                       ');
+    console.log(' |  ____|               | |                      ');
+    console.log(' | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ '); 
+    console.log(' |  __| | `_ ` _ \| |_ \| |/ _ \| | | |/ _ \/ _ \ '); 
+    console.log(' | |____| | | | | | |_) | | (_) | |_| |  __/  __/'); 
+    console.log(' |______|_| |_| |_| .__/|_|\___/ \__, |\___|\___|');
+    console.log(' |  \/  |         | |             __/ |          ');
+    console.log(' | \  / | __ _ _ _|_| __ _  __ _ |___/_ __       '); 
+    console.log(' | |\/| |/ _` | `_ \ / _` |/ _` |/ _ \ `__|      '); 
+    console.log(' | |  | | (_| | | | | (_| | (_| |  __/ |         '); 
+    console.log(' |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|         '); 
+    console.log('                            __/ |                '); 
+    console.log('                           |___/                 '); 
+    
+    employeeTracker();
+
+})
 
 function employeeTracker() {
 
