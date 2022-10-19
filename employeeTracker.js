6bee5469185f826208ca627d2ee3f4e25454e2d8
Mitchell Foote <mcfoote321@gmail.com>
2022-10-18 23:04:03 -0400

    setup connection

--- employeeTracker.js
+++ employeeTracker.js
@@ -3,7 +3,11 @@ const sql = require('mysql2');
 const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_db"
 });
 
 function employeeTracker() {
